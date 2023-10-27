88e556bbc38817cf1e161e94b077ad7f38364640
Shoaib Mastan <114753586+shubsmastan@users.noreply.github.com>
2023-10-27 15:30:21 +0100

    Learning variables and shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x: i32 = 1;
+    x += 2;
+    let hi = "hi";
+
+    println!("The value of x is {}.", x);
+    println!("{}, world!", hi);
 }
